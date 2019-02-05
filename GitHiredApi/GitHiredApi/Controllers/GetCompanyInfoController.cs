dcdd2ae424a79c1371a04e7c411bbd44a8144068
xia LIU <xia.liu2@outlook.com>
2019-02-05 11:59:32 -0800

    updated controller

--- GitHiredApi/GitHiredApi/Controllers/GetCompanyInfoController.cs
+++ GitHiredApi/GitHiredApi/Controllers/GetCompanyInfoController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using GitHiredApi.Data;
 using GitHiredApi.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 
 namespace GitHiredApi.Controllers
 {
@@ -19,10 +21,22 @@ namespace GitHiredApi.Controllers
             _context = context;
         }
 
-        public IEnumerable<Company> GetCompaniess()
+        public async Task <ActionResult<IEnumerable<Company>>> GetCompanies()
         {
-            return _context.Companies.ToList();
+          var companies =await _context.Companies
+                .Include(r => r.Jobs)
+                .ToArrayAsync();
+            foreach (var item in companies)
+            {
+                //item.Jobs = await _context.Jobs.Where(jo => jo.CompanyID == item.ID).ToArrayAsync();
+                item.Jobs = item.Jobs.ToArray();
+
+            }
+
+            return Ok(new { results=companies });
         }
+
+    
 
         // GET: api/Todo/5
         [HttpGet("{id}")]
