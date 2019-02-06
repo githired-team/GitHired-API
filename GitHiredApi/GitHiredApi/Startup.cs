ba880533a72abdd0292728c1d67dea830b07b4b4
RickFlinn <mriodine@gmail.com>
2019-02-05 23:45:54 -0800

    Added NSwag nuget package. Added swagger document builder to injected services in startup. Added UseSwagger and UseSwaggerUi3 to injected application builder in startup. API is now ready to use with the Swagger UI.

--- GitHiredApi/GitHiredApi/Startup.cs
+++ GitHiredApi/GitHiredApi/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using NJsonSchema;
+using NSwag.AspNetCore;
 
 namespace GitHiredApi
 {
@@ -33,6 +35,8 @@ namespace GitHiredApi
 
             services.AddDbContext<GitHiredApiDbContext>(options =>
             options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
+
+            services.AddSwaggerDocument();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,8 +52,15 @@ namespace GitHiredApi
                 app.UseHsts();
             }
 
+
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            // Swagger UI middleware - facilitates using Swagger to generate documentation for our API as a GUI
+            app.UseSwagger();
+            app.UseSwaggerUi3();
+
+
         }
     }
 }
